67583b36bd7949781f62c9870dc245e8f3c55d05
NZhukov <nzhukov.rnd@gmail.com>
2023-07-16 14:31:35 +0400

    create storage

---
+++ To-Do manager/Model/TaskStorage.swift
@@ -0,0 +1,33 @@
+//
+//  TaskStorage.swift
+//  To-Do manager
+//
+//  Created by Nikolay Zhukov on 16.07.2023.
+//
+
+import Foundation
+
+protocol TaskStorageProtocol {
+    func loadTasks() -> [TaskProtocol]
+    func saveTasks(_ tasks: [TaskProtocol])
+}
+
+class TaskStorage: TaskStorageProtocol {
+    func loadTasks() -> [TaskProtocol] {
+        // hdеменная реализация
+        let testTasks: [TaskProtocol] = [
+            Task(title: "Buy milk", type: .normal, status: .planned),
+            Task(title: "Wash kat", type: .important, status: .planned),
+            Task(title: "Buy new vacuumcleaner", type: .important, status: .complited),
+            Task(title: "Learn SWIFT", type: .important, status: .complited),
+            Task(title: "Call parents", type: .important, status: .planned)
+        ]
+        return testTasks
+    }
+    
+    func saveTasks(_ tasks: [TaskProtocol]) {
+        <#code#>
+    }
+    
+    
+}
