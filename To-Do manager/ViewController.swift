922167575c7060399587a84fd3db3cc2a2dbc7a8
NZhukov <nzhukov.rnd@gmail.com>
2023-07-16 14:04:47 +0400

    Initial Commit

---
+++ To-Do manager/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  To-Do manager
+//
+//  Created by Nikolay Zhukov on 16.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
