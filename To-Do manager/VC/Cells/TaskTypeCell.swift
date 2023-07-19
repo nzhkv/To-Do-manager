0f00a4d51ef0fe0f6b7e080d1e2f3693d73947bf
NZhukov <nzhukov.rnd@gmail.com>
2023-07-19 16:15:23 +0400

    save

---
+++ To-Do manager/VC/Cells/TaskTypeCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTypeCell.swift
+//  To-Do manager
+//
+//  Created by Nikolay Zhukov on 19.07.2023.
+//
+
+import UIKit
+
+class TaskTypeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
